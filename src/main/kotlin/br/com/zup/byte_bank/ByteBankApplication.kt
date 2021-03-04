fdbf862439a06baaaf4ae8566584785e6a1724fc
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-04 15:57:02 -0300

    Trabalhando com tipos de variaveis e operacoes aritiméticas

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -1,5 +1,23 @@
 package br.com.zup.byte_bank
 
 fun main() {
-    print("Bem vindo ao Byte Bank!")
+    println("Bem vindo ao Byte Bank!")
+
+    // O kotlin identifica implicitamente o tipo da variavel, nesse caso, uma String
+    val titular = "Rodrigo"
+
+    // Para deixar explicito o tipo da variavel, é da seguinte forma
+    val numeroConta: Int = 1000
+
+    // Para uma variavel que pode ser alterada, utilizamos o var, quando não vai ser alterada, usamos o val
+    var saldoConta = 0.0
+    saldoConta = 200.0
+
+    // O Kotlin altera automaticamente a expressao "saldoConta = saldoConta + 100 para a expressao abaixo
+    // seguindo a boa pratica
+    saldoConta += 100
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo da conta: $saldoConta")
 }
