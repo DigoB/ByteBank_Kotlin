0adf6b73564c887970be0c7bd9a8ed6dffd00f08
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-05 12:48:53 -0300

    Criada a classe Gerente com funcao autenticar

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -7,7 +7,6 @@ fun main() {
         nome = "Rodrigo",
         cpf = "111.111.111-11",
         salario = 1000.0,
-        tipo = 0
     )
 
     println("Nome: ${funcionario.nome}")
@@ -15,4 +14,23 @@ fun main() {
     println("Salario: ${funcionario.salario}")
 
     println("Bonificacao: ${funcionario.bonificacao()}")
+
+    val gerente = Gerente(
+        nome = "Alguem",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+    println ("Nome: ${gerente.nome}")
+    println ("Cpf: ${gerente.cpf}")
+    println ("Salario: ${gerente.salario}")
+
+    println ("Bonificacao: ${gerente.bonificacao()}")
+
+    if (gerente.autentica(1234)){
+        println("Autenticação feita com sucesso!")
+    } else {
+        println("Falha na autenticação!")
+    }
+
 }
