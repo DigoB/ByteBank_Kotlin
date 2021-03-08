37135352720639d9ffd1c286af758b2d8c2a8b0a
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-08 20:44:24 -0300

    Criando variável global

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -1,7 +1,36 @@
 package br.com.zup.byte_bank
 
+import br.com.zup.byte_bank.contas.*
+import jdk.internal.vm.compiler.word.LocationIdentity.init
+
 fun main() {
 
     testaContasDiferentes()
+
+    val contaCorrente = ContaCorrente(
+        titular = Cliente(nome = "Rodrigo", cpf = "111.111.111-11", senha = 1),
+        numeroConta = 1000
+    )
+
+    val contaPoupanca = ContaPoupanca(
+        Cliente(
+            nome = "Rodrigo",
+            cpf = "111.111.111-11",
+            senha = 1,
+            endereco = Endereco(
+                logradouro = "Rua A",
+            )
+        ), numeroConta = 1001
+    )
+
+    val contaSalario = ContaSalario(
+        Cliente(
+            nome = "Alguem",
+            cpf = "222.222.222-22",
+            senha = 2
+        ), numeroConta = 1002
+    )
+
+    println("Total de contas: $totalContas")
 
 }
--- src/main/kotlin/br/com/zup/byte_bank/contas/Conta.kt
+++ src/main/kotlin/br/com/zup/byte_bank/contas/Conta.kt
@@ -1,5 +1,10 @@
 package br.com.zup.byte_bank.contas
 
+// Variavel global, pode ser acessada e alterada em qualquer ponto do projeto
+var totalContas = 0
+    // Se for declarada como privado, será alterada globalmente dentro do arquivo, por todas as classes que ele tiver.
+    private set
+
 // No Kotlin, ao declarar o construtor, é possivel fazer diretamente declarando também as variaveis
 abstract class Conta(
     val titular: Cliente,
@@ -12,6 +17,10 @@ abstract class Conta(
         if (valor > 0) {
             saldoConta += valor
         }
+    }
+
+    init {
+        totalContas++
     }
 
     abstract fun saca(valor: Double)
