4426c6dada70482b8b67ebad7f7b7c1fcc036691
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-09 17:20:03 -0300

    Trabalhando com try e if como expressoes

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -1,32 +1,28 @@
 package br.com.zup.byte_bank
 
-import br.com.zup.byte_bank.contas.Endereco
-import java.lang.ClassCastException
-
 fun main() {
-    println("Inicio da funcao main")
-    funcao1()
-    println("Fim da funcao main")
-}
 
-fun funcao1() {
-    println("Inicio da funcao 1")
-    funcao2()
-    println("Fim da funcao 1")
-}
+    val entrada: String = "1.0"
 
-fun funcao2() {
-    try {
-        for (i in 1..5) {
-            println(i)
-            val endereco = Any()
-            endereco as Endereco
-        }
-    } catch (e: ClassCastException) {
-        println("Nao é possivel fazer cast da classe!")
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversao")
+        e.printStackTrace()
+        null
     }
 
-    println("Fim da funcao 2")
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("Valor recebido: $valorComTaxa")
+    } else {
+        println("Valor invalido")
+    }
 }
 
 
