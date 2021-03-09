a5cad56c3d1a1640a730137ed8d51ad897360a95
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-09 16:51:53 -0300

    Tratamento de exceção ClassCastException

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -1,10 +1,35 @@
 package br.com.zup.byte_bank
 
 import br.com.zup.byte_bank.contas.Endereco
+import java.lang.ClassCastException
 
 fun main() {
-    testaContasDiferentes()
+    println("Inicio da funcao main")
+    funcao1()
+    println("Fim da funcao main")
 }
+
+fun funcao1() {
+    println("Inicio da funcao 1")
+    funcao2()
+    println("Fim da funcao 1")
+}
+
+fun funcao2() {
+    try {
+        for (i in 1..5) {
+            println(i)
+            val endereco = Any()
+            endereco as Endereco
+        }
+    } catch (e: ClassCastException) {
+        println("Nao é possivel fazer cast da classe!")
+    }
+
+    println("Fim da funcao 2")
+}
+
+
 
 
 
