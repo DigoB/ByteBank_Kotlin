1ca84e33f34732c62b4a80e793af5b90ef2d83f5
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-09 11:32:31 -0300

    Object Declaration usando Companion Object

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -5,20 +5,19 @@ import br.com.zup.byte_bank.funcionarios.Autenticavel
 
 fun main() {
 
-    // Object expression, traz a possibilidade de criar um objeto sem criar a classe, cria algo como um construtor
-    // sem precisar de uma classe especifica, é uma "classe anonima"
-    val objeto = object : Autenticavel {
-        val nome: String = "Alguem"
-        val cpf: String = "111.111.111-11"
-        val senha: Int = 1
+    // Correcao do push anterior, o que é considerado uma classe anonima é o object declaration, não expression
 
-        override fun autentica(senha: Int) = this.senha == senha
-    }
-
-    println("Nome cliente: ${objeto.nome}")
-    println("Cpf cliente: ${objeto.cpf}")
-    println("------------------------------------------------")
-
+//    val objeto = object : Autenticavel {
+//        val nome: String = "Alguem"
+//        val cpf: String = "111.111.111-11"
+//        val senha: Int = 1
+//
+//        override fun autentica(senha: Int) = this.senha == senha
+//    }
+//
+//    println("Nome cliente: ${objeto.nome}")
+//    println("Cpf cliente: ${objeto.cpf}")
+//    println("------------------------------------------------")
 
 
     testaContasDiferentes()
@@ -46,5 +45,8 @@ fun main() {
             senha = 2
         ), numeroConta = 1002
     )
+
+    println("-----------------------------------------------")
+    println("Total de contas: ${Conta.total}")
 
 }
--- src/main/kotlin/br/com/zup/byte_bank/contas/Conta.kt
+++ src/main/kotlin/br/com/zup/byte_bank/contas/Conta.kt
@@ -8,6 +8,14 @@ abstract class Conta(
     var saldoConta = 0.0
         protected set
 
+    // Companion Object é uma object declaration que faz possivel com que o objeto seja acessivel nas classes filhas
+    // porém, só pode ser alterado na classe mae
+    companion object Contador {
+        var total = 0
+            private set
+
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) {
             saldoConta += valor
