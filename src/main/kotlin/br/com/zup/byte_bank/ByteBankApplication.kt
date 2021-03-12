2766d8132b6c417e7273e32a78c6217dfbb974a7
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-12 15:53:38 -0300

    Mesma operacao com funcao aninoma e expressao lambda

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -5,13 +5,20 @@ import br.com.zup.byte_bank.functionTypes.testaFunctionTypeReferencia
 
 fun main() {
 
-//    // Criando função anonima
-//    // A criação é feita com o fun() depois de declarar o retorno, nesse caso, Unit
-//    val minhaFuncaoLambda: () -> Unit = fun() {
-//        println("Executa como lambda")
-//    }
-//    // Parenteses iniciando a variável transformada em função
-//    println(minhaFuncaoLambda())
+//     Criando função anonima
+//     A criação é feita com o fun() depois de declarar o retorno, nesse caso, Unit
+    val minhaFuncaoAnonima = fun(a: Int, b: Int): Int {
+        return a + b
+    }
+    println("Soma com funcao anonima: ${minhaFuncaoAnonima(20,10)}")
+
+    // faz a mesma coisa que a função anonima, mas é menos expressiva
+    // a, b são os apelidos dos numeros q são somados
+    val minhaFuncaoLambda: (Int, Int) -> Int = { a, b ->
+        a + b
+    }
+    println("Soma com expressão lambda: ${minhaFuncaoLambda(15, 10)}")
+    // Parenteses iniciando a variável transformada em função
 
     testaFunctionTypeReferencia()
     testaFunctionTypeClasse()
