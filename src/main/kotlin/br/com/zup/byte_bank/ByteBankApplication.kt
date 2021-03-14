8ed6669eb9ed830bbe1e77d3f74ae343551e7ca6
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-03-14 15:55:29 -0300

    Scope Function Run

--- src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
+++ src/main/kotlin/br/com/zup/byte_bank/ByteBankApplication.kt
@@ -1,9 +1,41 @@
 package br.com.zup.byte_bank
 
+import br.com.zup.byte_bank.contas.Cliente
+import br.com.zup.byte_bank.contas.ContaPoupanca
 import br.com.zup.byte_bank.contas.Endereco
 
 fun main() {
 
+
+}
+
+fun testaRun() {
+    // Conta poupança tendo rendimento mensal
+
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("Taxa mensal: $taxaMensal")
+
+
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "ALguem", cpf = "111.111.111-11", senha = 1234), 1000)
+    contaPoupanca.run {
+        deposita(1000.0)
+        saldoConta * taxaMensal
+    }.let { rendimentoMensal ->
+        println("Rendimento mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo: Double = contaPoupanca.saldoConta
+        repeat(12) { indice ->
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Rendimento anual: $rendimentoAnual")
+}
+
+fun testaWtih() {
     with(Endereco()) {
         logradouro = "Rua A"
         numero = 1000
@@ -14,9 +46,9 @@ fun main() {
         completo()
     }.let { enderecoCompleto ->
         println(enderecoCompleto)
-
     }
 
+    run { }
 }
 
 fun testaLet() {
